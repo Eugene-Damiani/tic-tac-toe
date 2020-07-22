4999407702012706d3e9764b85c12bd72d987177
Eugene-Damiani <damianieugene@gmail.com>
2020-07-22 07:38:12 -0500

    Config changes

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -3,12 +3,10 @@
 let apiUrl
 const apiUrls = {
   production: 'https://tic-tac-toe-api-production.herokuapp.com',
-development: 'https://tic-tac-toe-api-development.herokuapp.com'
+  development: 'https://tic-tac-toe-api-development.herokuapp.com'
 // 'http://localhost:4741'
 // development: 'https://tic-tac-toe-api-development.herokuapp.com'
-
 }
-
 if (window.location.hostname === 'localhost') {
   apiUrl = apiUrls.development
 } else {
