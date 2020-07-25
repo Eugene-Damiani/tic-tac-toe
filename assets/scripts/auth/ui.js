d97d943121ec753c3c093fa17a0d8d92ac422b5d
Eugene-Damiani <damianieugene@gmail.com>
2020-07-25 12:22:50 -0500

    Removed bugs

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -4,10 +4,12 @@ const store = require('../store')
 
 const signUpSuccess = function () {
   $('#message').text('Successfully Signed Up!')
+  $('form').trigger('reset')
 }
 
 const signUpFailure = function() {
   $('#message').text('Missing Infomation Try Again')
+  $('form').trigger('reset')
 }
 
 const signInSuccess = function (response) {
@@ -17,26 +19,33 @@ const signInSuccess = function (response) {
   console.log('store ', store)
   $('#authenticated').show()
   $('#unauthenticated').hide()
+  $('form').trigger('reset')
 }
 
-const signInFailure = function (){
+const signInFailure = function () {
   $('#message').text('Your password or username is incorrect')
+  $('form').trigger('reset')
 }
 
-const changePasswordSuccess = function (){
+const changePasswordSuccess = function () {
   $('#message').text('Your password has been changed')
+  $('form').trigger('reset')
 }
 
-const changePasswordFailure = function (){
+const changePasswordFailure = function () {
   $('#message').text('Your new passowrd does not match')
+  $('form').trigger('reset')
 }
 
-const signOutSuccess = function(){
+const signOutSuccess = function () {
   $('#message').text('You have signed out')
+  store.user = null
+  $('form').trigger('reset')
 }
 
-const signOutFailure = function(){
+const signOutFailure = function () {
   $('#message').text('You are still signed in')
+
 }
 
 module.exports = {
--- curl-scripts/sign-out.sh
+++ curl-scripts/sign-out.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 curl "https://tic-tac-toe-api-production.herokuapp.com/sign-out" \
 --include \
 --request DELETE \
---header "Content-Type: application json" \
---header "Authorization: Bearer ${TOKEN}"
+--header "Authorization: Bearer ${TOKEN}" \
+--header "Content-Type: application/json"
+
 
 echo
